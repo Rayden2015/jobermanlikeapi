dd433b2268519c44e31e1ed13f4f703b5fb80da3
= <nurundin2010@gmail.com>
2020-05-21 23:09:31 +0000

    Update and Delete Jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 const Job = require('../models/jobs');
+const geoCoder = require('../utils/geocoder');
 
 //Get all jobs => /api/v1/jobs
 exports.getJobs = async (req, res, next ) => {
@@ -17,12 +18,97 @@ exports.newJob = async (req, res, next) => {
     console.log('Add New Job | Request Body : ');
     console.log(req.body);
 
-    const job = await Job.create(req.body);
+    const jobs = await Job.create(req.body);
    
     res.status('200').json({
         success: true,
         message: 'Job Created',
-        data: job
+        data: jobs
     })
 
+}
+
+
+//Search for jobs withing a city and radius => /api/v1/jobs/:zipcode/:distance
+exports.getJobsInRadius = async (req, res, next) => {
+    console.log("Jobs Controller | getJobsInRadius");
+    const { zipcode, distance } = req.params;
+
+    //Getting latitude and longitude from geocoder
+    const loc = await geoCoder.geocode(zipcode);
+    const latitude = loc[0].latitude;
+    const longitude = loc[0].longitude;
+    const radius = distance/ 3963;
+
+    try{
+        const jobs = await Job.find({
+            location: {$geoWithin: {$centerSphere: [[longitude, latitude], radius]}}
+        });
+
+        res.status(200).json({
+            success: true,
+            results: jobs.length,
+            data: jobs
+        });
+
+    }catch(e){
+        console.log("Jobs Controller | getJobsInRadius : " + e.message);
+    }
+
+}
+
+
+//Update Job
+exports.updateJob = async (req, res, next) => {
+    try{
+        let job = await Job.findById(req.params.id);
+        if(!job){
+            return res.status(404).json({
+                success: false,
+                message: 'Job not found.'
+            });
+        }
+    
+        job = await Job.findByIdAndUpdate(req.params.id,req.body);
+    
+        res.status(200).json({
+            success: true,
+            message : 'Job Updated Successfully',
+            data: job
+    });
+   
+    }catch(e){
+        console.error('Update Job | error : '); 
+        console.error(e.message);
+    }
+}
+
+
+//Deleting a job => /api/v1/job/:id
+exports.deleteJob = async (req, res, next) => {
+    try{
+        let job = await Job.findById(req.params.id);
+        if(!job){
+            return res.status(404).json({
+                success: false,
+                message: 'Job not found.'
+            });
+        }
+    
+        job = await Job.findByIdAndDelete(req.params.id,req.body,{
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+    
+        res.status(200).json({
+            success: true,
+            message : 'Job Deleted Successfully',
+            data: job
+    });
+   
+    }catch(e){
+        console.error('Update Job | error : '); 
+        console.error(e.message);
+    }
 }
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 //Importing Jobs Controller
-const { getJobs, newJob } =require('../controllers/jobsController');
+const { getJobs, newJob, getJobsInRadius, updateJob, deleteJob } =require('../controllers/jobsController');
 
 
 //Assinging jobs controller to route
-router.route('/jobs').get(getJobs);
-router.route('/job/new').post(newJob);
+router.route('/jobs').get(getJobs); //Getting all jobs
+router.route('/job/new').post(newJob); //Creating new job
+router.route('/jobs/:zipcode/:distance').get(getJobsInRadius); //searching for jobs withing a radius of a zipcode
+router.route('/job/:id')
+        .put(updateJob)
+        .delete(deleteJob);
+
+
 
 module.exports = router;
 
