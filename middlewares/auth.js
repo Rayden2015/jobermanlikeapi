1c53a61f9a7b20a73ce88c5448f04f7b7f5c7219
= <nurundin2010@gmail.com>
2020-07-04 07:51:57 +0000

    Authorise user roles and permissions

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -26,4 +26,12 @@ exports.isAuthenticatedUser = catchAsyncErrors(async(req, res, next) => {
 });
 
 
-
+//Handling user's roles
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role(${req.user.role}) is not allowed to access this resource`, 403));
+        }
+        next();
+    }
+}
--- routes/jobs.js
+++ routes/jobs.js
@@ -4,15 +4,15 @@ const router = express.Router();
 //Importing Jobs Controller
 const { getJobs, newJob, getJobsInRadius, updateJob, deleteJob, getJob, jobStats } = require('../controllers/jobsController');
 
-const {isAuthenticatedUser} = require('../middlewares/auth');
+const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
 //Assinging jobs controller to route
 router.route('/jobs').get(getJobs); //Getting all jobs
-router.route('/job/new').post(isAuthenticatedUser, newJob); //Creating new job
+router.route('/job/new').post(isAuthenticatedUser, authorizeRoles('employer','admin'), newJob); //Creating new job
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius); //searching for jobs withing a radius of a zipcode
 router.route('/job/:id')
-        .put(isAuthenticatedUser, updateJob)
-        .delete(isAuthenticatedUser, deleteJob);
+        .put(isAuthenticatedUser,authorizeRoles('employer','admin'), updateJob)
+        .delete(isAuthenticatedUser,authorizeRoles('employer','admin'), deleteJob);
 router.route('/job/:id').get(getJob); //Getting a single job single the id and or slug
 router.route('/job/stats/:topic').get(jobStats); //Getting job statistics given the job(topic)
 
