1057330c27b8ecb4c5f16a1bf40009af9269af2d
= <nurundin2010@gmail.com>
2020-10-21 17:05:56 +0000

    Delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,10 +49,32 @@ exports.updateUser = catchAsyncErrors(async(req, res, next) => {
         role    : req.body.role
     }
 
-    const user = await User.findByIdAndUpdate(req.user.id,newUserData, {runValidators: true});
+    const user = await User.findByIdAndUpdate(req.user.id,newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
 
     res.status(200).json({
         success: true,
         data: user
     });
+});
+
+
+//Delete user data => /api/v1/me/delete
+exports.deleteUser = catchAsyncErrors(async(req, res, next) => {
+    const user = await User.findByIdAndDelete(req.user.id);
+    console.log('userController | deleteUser | '+ req.user);
+
+    res.cookie('token', 'none', {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'User Deleted Succesfully'
+    });
+
 });
